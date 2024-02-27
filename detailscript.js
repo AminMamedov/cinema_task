d3f1d4312621e7d1e6bfd0e4636745df965c9108
Mammadov Amin <aminrm@code.local>
2024-02-27 17:55:22 +0400

    Fixedsearchbar

--- detailscript.js
+++ detailscript.js
@@ -7,7 +7,7 @@ fetch(`https://api.tvmaze.com/shows/${id}`)
     console.log(data);
     let parent = document.getElementById("detailCard")
         
-            parent.innerHTML+=` <div  class="card mb-3" style="max-width: 800px;">
+            parent.innerHTML+=` <div id="detaillll" class="card mb-3" style="max-width: 1500px;">
             <div id="imageText" class="row g-0">
               <div id="image" class="col-md-4">
                 <img src="${data.image.original}" class="img-fluid rounded-start" alt="...">
@@ -15,8 +15,18 @@ fetch(`https://api.tvmaze.com/shows/${id}`)
               <div id="text" class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title">${data.name}</h5>
-                  <p class="card-text">${data.name}</p>
-                  <a href="#" class="card-link">${data.name}</a>
+                  <p class="card-text">${data.summary}</p>
+                  <ul>
+                    <li>IMBD rating: ${data.rating.average}</li>
+                    <li>Language: ${data.language}</li>
+                    <li>Genre: ${data.genres} </li>
+                    <li>Premiered: ${data.premiered}</li>
+                    <li>Ended: ${data.ended}</li>
+                  </ul>
+                  <button><a href="${data.officialSite}">Go to website</a></button>
+                  <button><a href="cinema.html">Go back</a></button>
+
+
                 </div>
               </div>
             </div>
--- script.js
+++ script.js
@@ -1,34 +1,86 @@
 
-async function GetMovies() {
-    let movies = await fetch("https://api.tvmaze.com/shows");
-    let data = await movies.json();
-    return data
+// async function GetMovies() {
+//     let movies = await fetch("https://api.tvmaze.com/shows");
+//     let data = await movies.json();
+//     return data
 
-}
+// }
+// let text = document.getElementById("searchimput").value.toLowerCase().trim()
+// window.onload = async () => {
+//     let moviess = await GetMovies()                  
+//     let parent = document.getElementById("cinemaMainCardss")
+//     parent.innerHTML=""
+//     moviess.forEach(movie => {if(movie.name.toLowerCase().includes(text)){
+//       parent.innerHTML += `
+//         <div class="card" style="width: 18rem;" >
+// <img src="${movie.image["medium"]}" class="card-img-top" alt="...">
+// <div class="card-body">
+//   <h5 class="card-title">Movie Name: ${movie["name"]}</h5>
+//   <p class="card-text">Premiered at: ${movie["premiered"]}</p>
+// </div>
+// <ul class="list-group list-group-flush">
+//   <li class="list-group-item">${movie["type"]}</li>
+//   <li class="list-group-item">Language: ${movie["language"]}</li>
+// </ul>
+// <div class="card-body">
+//   <a href="${movie["officialSite"]}" class="card-link">Go to website</a>
+//   <a href="detail.html?detailid=${movie["id"]}" class="card-link">Another link</a>
+// </div>
+// </div>
 
-window.onload = async () => {
-    let moviess = await GetMovies()
-    let parent = document.getElementById("cinemaMainCardss")
-    moviess.forEach(movie => {
-        parent.innerHTML += `
-        <div class="card" style="width: 18rem;" >
-<img src="${movie.image["medium"]}" class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">Movie Name: ${movie["name"]}</h5>
-  <p class="card-text">Premiered at: ${movie["premiered"]}</p>
-</div>
-<ul class="list-group list-group-flush">
-  <li class="list-group-item">${movie["type"]}</li>
-  <li class="list-group-item">Language: ${movie["language"]}</li>
-</ul>
-<div class="card-body">
-  <a href="${movie["officialSite"]}" class="card-link">Go to website</a>
-  <a href="detail.html?detailid=${movie["id"]}" class="card-link">Another link</a>
-</div>
-</div>
-        
-        `
+//         `
+//     }
 
+
+
+//     });
+// }
+
+
+
+
+
+fetch("https://api.tvmaze.com/shows")
+  .then(res => res.json())
+  .then(data => {
+    let isSearch = false;
+
+    let searchBtn = document.getElementById("searchBtn");
+    searchBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      let text = document.getElementById("searchimput").value.toLowerCase().trim();
+      console.log("ovveride", text);
+      let overData = data.filter(movie => movie.name.toLowerCase().includes(text));
+      isSearch = true;
+      showMovies(overData);
     });
-}
+
+    function showMovies(data) {
+      let parent = document.getElementById("cinemaMainCardss")
+      parent.innerHTML = "";
+      data.forEach(movie => {
+        parent.innerHTML += `
+      <div class="card" style="width: 18rem;" >
+          <img src="${movie.image["medium"]}" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">Movie Name: ${movie["name"]}</h5>
+          <p class="card-text">Premiered at: ${movie["premiered"]}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">${movie["type"]}</li>
+          <li class="list-group-item">Language: ${movie["language"]}</li>
+          </ul>
+          <div class="card-body">
+          <a href="${movie["officialSite"]}" class="card-link">Go to website</a>
+          <a href="detail.html?detailid=${movie["id"]}" class="card-link">Another link</a>
+          </div>
+      </div>`
+      });
+    };
+
+    if (isSearch===false) {
+      showMovies(data);
+    }
+  })
+
 
